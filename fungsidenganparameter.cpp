f55165a51f7ec9c7bcdf7a5ff91fab42f5e707ce
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-03-02 22:09:00 +0700

    program utama

--- fungsidenganparameter.cpp
+++ fungsidenganparameter.cpp
@@ -12,3 +12,21 @@ void input(){
     cout << "Masukkan Lebar : ";
     cin >> lebar;
 }
+
+int luasPersegi(int a, int b){
+    return a * b;
+}
+
+int jumlah(int x, int y, int z){
+    return x + y + z;
+}
+
+void output(){
+    cout << "Hasilnya : " << luasPersegi(panjang, lebar) << endl;
+}
+
+int main(){ // mulai
+    input();
+    output();
+    cout << "Hasil dari Penjumlahan : " << jumlah(3, 2, 7);
+} // selesai
