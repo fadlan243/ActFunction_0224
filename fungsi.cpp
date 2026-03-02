c0c10278d4cea3f198b7a23a1f4c5e4572333f81
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-03-02 22:07:34 +0700

    implementasi fungsi dan prosedur

--- fungsi.cpp
+++ fungsi.cpp
@@ -5,3 +5,11 @@ using namespace std;
 // deklarasi variabel
 int p, l;
 
+// implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+}
+
