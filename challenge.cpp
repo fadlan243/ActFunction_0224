c0afda7c7092e939825ffb483586a16e0af54ff3
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-03-02 22:05:34 +0700

    implementasi prosedur dan fungsi

--- challenge.cpp
+++ challenge.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;
+
+void input(float &d1, float &d2) {
+    cout << "Masukkan diagonal 1: ";
+    cin >> d1;
+    cout << "Masukkan diagonal 2: ";
+    cin >> d2;
+}
