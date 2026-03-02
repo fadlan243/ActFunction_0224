4de3fca17a1556924bcd4b1ed699bfda0827ad0f
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-03-02 21:36:07 +0700

    first commit

---
+++ challenge.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void input(float &d1, float &d2) {
+    cout << "Masukkan diagonal 1: ";
+    cin >> d1;
+    cout << "Masukkan diagonal 2: ";
+    cin >> d2;
+}
+
+float hitungLuas(float d1, float d2) {
+    return 1/2 * d1 * d2;
+}
+
+void output(float luas) {
+    cout << "Luas belah ketupat adalah: " << luas << endl;
+}
+
+int main() {
+    float d1, d2, luas;
+
+    input(d1, d2);              
+    luas = hitungLuas(d1, d2);  
+    output(luas);               
+
+    return 0;
+}
